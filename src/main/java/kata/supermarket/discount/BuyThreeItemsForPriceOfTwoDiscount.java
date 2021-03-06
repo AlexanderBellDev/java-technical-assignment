f116ddda850dc7343e74cccb8baa6ba129c39e8a
Alex <51273561+AlexanderBellDev@users.noreply.github.com>
2021-03-06 17:58:26 +0000

    Buy three get one free discount implemented + unit test

---
+++ src/main/java/kata/supermarket/discount/BuyThreeItemsForPriceOfTwoDiscount.java
@@ -0,0 +1,37 @@
+package kata.supermarket.discount;
+
+import kata.supermarket.Item;
+
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class BuyThreeItemsForPriceOfTwoDiscount extends Discount {
+
+    public BuyThreeItemsForPriceOfTwoDiscount() {
+    }
+
+    public BuyThreeItemsForPriceOfTwoDiscount(List<Item> currentBasket) {
+        super(currentBasket);
+    }
+
+    @Override
+    public BigDecimal calculateDiscount() {
+        if(currentBasket.size() < 2) return BigDecimal.ZERO;
+
+        Map<BigDecimal, List<Item>> itemByPriceMap = currentBasket.stream()
+                .collect(Collectors.groupingBy(Item::price));
+
+        TreeMap<BigDecimal, List<Item>> ascendingOrderMap = new TreeMap<>();
+        itemByPriceMap.forEach(ascendingOrderMap::put);
+
+        for (BigDecimal prices : ascendingOrderMap.keySet()) {
+            if(ascendingOrderMap.get(prices).size() >= 3){
+                return prices;
+            }
+        }
+
+        //if no items exist with the quantity of three
+        return BigDecimal.ZERO;
+    }
+}
--- src/test/java/kata/supermarket/DiscountTest.java
+++ src/test/java/kata/supermarket/DiscountTest.java
@@ -1,6 +1,7 @@
 package kata.supermarket;
 
 import kata.supermarket.discount.BuyOneGetOneFreeDiscount;
+import kata.supermarket.discount.BuyThreeItemsForPriceOfTwoDiscount;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -12,15 +13,32 @@ class DiscountTest {
     @Test
     void buyOneGetOneFreeTest(){
         Product product = new Product(BigDecimal.valueOf(15.00));
-        Item item = product.oneOf();
         WeighedProduct weighedProduct = new WeighedProduct(BigDecimal.valueOf(1.00));
 
         Basket basket = new Basket();
         basket.addDiscount(new BuyOneGetOneFreeDiscount());
-        basket.add(item);
+        basket.add(product.oneOf());
         basket.add(weighedProduct.weighing(BigDecimal.valueOf(20)));
 
         assertEquals(BigDecimal.valueOf(20).doubleValue(),basket.total().doubleValue());
+    }
+
+    @Test
+    void buyThreeItemsForPriceOfTwoTest(){
+        Product productOne = new Product(BigDecimal.valueOf(15.00));
+        Product productTwo = new Product(BigDecimal.valueOf(5.00));
+
+
+        Basket basket = new Basket();
+        basket.addDiscount(new BuyThreeItemsForPriceOfTwoDiscount());
+        basket.add(productOne.oneOf());
+        basket.add(productOne.oneOf());
+        basket.add(productOne.oneOf());
+        basket.add(productTwo.oneOf());
+        basket.add(productTwo.oneOf());
+        basket.add(productTwo.oneOf());
+
+        assertEquals(BigDecimal.valueOf(55).doubleValue(),basket.total().doubleValue());
     }
 
 }
